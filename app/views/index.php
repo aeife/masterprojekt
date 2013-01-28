5b31b0b7f1f1d07acf1703395b2ce148babf82c3
André Eife <andre.eife@gmail.com>
2013-01-28 15:30:55 +0100

    register php

--- app/views/index.php
+++ app/views/index.php
@@ -6,6 +6,7 @@
     <body>
        <a href = "game">Play As Guest</a> <br/>
        <a href = "login">Play As User</a> <br/>
+       <a href = "register">Register</a> <br/>
        <a href = "score">Game Archive</a> <br/>
        <a href = "score/players">Player Highscore</a>
     </body>
